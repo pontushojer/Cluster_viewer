866c3bd51194b1d5c725461f8534f5af26b46fa5
pontushojer <pontushojer@gmail.com>
2018-02-15 11:01:42 +0100

    Update file input and y-axis manuvering.

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
 # Cluster viewer
 Visualize cluster data from ChIB or WFA experiments in browser.
-
+Data should be in bed format with cluster id in the forth column. Example:
+```
+chr16	71461594	71461783	33254	2	+
+chr2	210093832	210093994	33254	8	+
+chr7	128739812	128740098	33010	10	+
+...
+``
 
 To start bokeh server move into directory:
 ```
-cd path/to/Cluster_viewer
+cd /path/to/Cluster_viewer
 ```
 
 and type the following command
 ``` 
-bokeh serve .
+bokeh serve . --show
+```
+
+Data is required to be located or linked-to in the "data" folder.
+Alternatively the following command can be used to input data directly.
+```
+bokeh serve . --show --arg /path/to/bed_file
 ```
 
 Copy the URL e.g (http://localhost:5006/Cluster_viewer or similar) into your browser.
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import argparse
 import pandas as pd
 import numpy as np
 from bokeh.io import show
-from bokeh.models import ColumnDataSource, HoverTool, Button, Range1d
+from bokeh.models import ColumnDataSource, HoverTool, Button, Range1d, TextInput
 from bokeh.plotting import figure
 from bokeh.layouts import layout
 from bokeh.io import curdoc
 import sys
+import os
+from time import localtime, strftime
 
 
 def chrom_number(chrom):
@@ -25,12 +27,17 @@ def chrom_number(chrom):
             return None
 
 
-def genome_file_handling(genome_file):
+def genome_file_handling(chromosome_file):
+    """
+    Read through genome file to get lengths of chromosomes and get an ordered list of chromosome for x-axis
+    """
+
     genome_lens = {}
-    with open(genome_file, 'r') as genome:
+    with open(chromosome_file, 'r') as genome:
         for entry in genome.readlines():
             chrom_name, chrom_len = entry.strip().split('\t')
 
+            # Skip unwanted chr
             if any(s in chrom_name for s in ['chrM', 'chrEBV', 'random', 'chrUn']):
                 continue
 
@@ -39,9 +46,9 @@ def genome_file_handling(genome_file):
     chrom_order = list(genome_lens.keys())
     chrom_order = sorted(chrom_order, key=lambda x: chrom_number(x))
 
+    # Create translation key that can set the relative start point for a particular chromosome
     translate_key = {}
     current_len = 0
-
     for chrom in chrom_order:
         translate_key[chrom] = current_len
         current_len += genome_lens[chrom]
@@ -49,69 +56,125 @@ def genome_file_handling(genome_file):
     return chrom_order, translate_key
 
 
-def get_cluster_df(bed_file, translate_key, chrom_order):
+def get_cluster_df(input_bed, translate_key, chrom_order):
+    """
+    Read through bed file to create dataframe with information.
+    """
     cluster_regions = []
 
-    with open(bed_file, 'r') as bed:
+    with open(input_bed, 'r') as bed:
         for interval in bed.readlines():
 
             chrom, start, end, cluster_id, weight, direction = interval.strip().split('\t')
             if chrom in chrom_order:
-                cluster_region = (int(cluster_id), str(chrom), int(start), int(end), int(start) + translate_key[chrom], int(end) + translate_key[chrom])
+                cluster_region = (int(cluster_id),
+                                  str(chrom),
+                                  int(start),
+                                  int(end),
+                                  int(start) + translate_key[chrom],
+                                  int(end) + translate_key[chrom])
+
                 cluster_regions.append(cluster_region)
 
     return pd.DataFrame(data=cluster_regions, columns=['Cluster', 'Chromosome', 'Start', 'End', 'xmin', 'xmax'])
 
 
-def sort_dataframe(dataframe, sort_type):
-    if sort_type == 'Cluster':
-        clusters = dataframe.Cluster.unique()
-        clusters = sorted(clusters, key=lambda x: int(x))
-        clusters_index = {cluster: i for i, cluster in enumerate(clusters)}
+def get_cluster_info(dataframe, phasing_threshold=100000, include_fragments=True, include_chromosomes=True,
+                     include_phasing=True):
+    """
+    Get basic cluster information from dataframe
+    """
 
-        dataframe['Index'] = pd.Series((np.zeros(len(dataframe)), 1))
-        for i, df_row in dataframe.iterrows():
-            dataframe.set_value(i, 'Index', clusters_index[df_row.Cluster])
+    cluster_data = []
+    # Loop over each cluster
+    for cluster in dataframe.Cluster.unique():
+        # Extract dataframe for cluster
+        cluster_dataframe = dataframe[dataframe.Cluster == cluster]
+
+        # Get number of fragments
+        fragments = 0
+        if include_fragments:
+            fragments = len(cluster_dataframe)
+
+        # Get number of chromosomes
+        chromosomes = 0
+        if include_chromosomes:
+            chromosomes = len(cluster_dataframe.Chromosome.unique())
+
+        # Get number of phased reads i.e pairs of neigbouring fragments within threshold
+        phasing = 0
+        if include_phasing:
+            if fragments > 1:
+                for chromosome in cluster_dataframe.Chromosome.unique():
+                    fragment_start_values = cluster_dataframe[cluster_dataframe.Chromosome == chromosome].Start
+                    if len(fragment_start_values) > 1:
+                        for start1, start2 in zip(fragment_start_values[:-1], fragment_start_values[1:]):
+                            if abs(start1 - start2) <= phasing_threshold:
+                                phasing += 1
+
+        # Store as tuple
+        data = (cluster, fragments, chromosomes, phasing)
+
+        # Apped to list
+        cluster_data.append(data)
+
+    return pd.DataFrame(data=cluster_data, columns=['Cluster', 'Fragments', 'Chromosomes', 'Phased_regions'])
+
+
+def sort_dataframe(dataframe, info_dataframe, sort_type):
+    """
+    Sort and index dataframe based on type of sort
+    """
+    ascending = True
+    if sort_type == 'Fragments' or sort_type == 'Phased regions':
+        ascending = False
+
+    clusters_index = {row.Cluster: nr for nr, (i, row) in enumerate(
+        info_dataframe.sort_values(by=sort_type, ascending=ascending).iterrows())}
+
+    dataframe['Index'] = pd.Series((np.zeros(len(dataframe)), 1))
+    for i, df_row in dataframe.iterrows():
+        dataframe.set_value(i, 'Index', clusters_index[df_row.Cluster])
 
     return dataframe.sort_values(by='Index'), clusters_index
 
 
-def main(bed_file, genome_file):
+def main(bed_file_name, genome_file_name, sort_cluster_type):
+    #
+    # Get initial information from bed file and genome file
+    #
 
-    chrom_order, translate_key = genome_file_handling(genome_file)
+    print(strftime("%Y-%m-%d %H:%M:%S ", localtime()), "Reading genome file: ", genome_file_name)
 
-    cluster_df = get_cluster_df(bed_file, translate_key, chrom_order)
+    chrom_order, translate_key = genome_file_handling(genome_file_name)
 
-    cluster_df_sorted, cluster_index = sort_dataframe(cluster_df, 'Cluster')
+    print(strftime("%Y-%m-%d %H:%M:%S ", localtime()), "Creating cluster dataframe from bed file: ", bed_file_name)
+
+    cluster_df = get_cluster_df(bed_file_name, translate_key, chrom_order)
+
+    print(strftime("%Y-%m-%d %H:%M:%S ", localtime()), "Collecting cluster info to dataframe")
+
+    cluster_info = get_cluster_info(cluster_df, sort_cluster_type,
+                                    include_chromosomes=(sort_cluster_type == 'Chromosomes'),
+                                    include_fragments=(sort_cluster_type == 'Fragments'),
+                                    include_phasing=(sort_cluster_type == 'Phased_regions'))
+
+    print(strftime("%Y-%m-%d %H:%M:%S ", localtime()), "Sorting cluster dataframe based on: ", sort_cluster_type)
+
+    cluster_df_sorted, cluster_index = sort_dataframe(cluster_df, cluster_info, sort_cluster_type)
 
     cluster_df_sorted.Cluster = cluster_df_sorted.Cluster.astype(str)
 
-    selection = cluster_df_sorted.loc[cluster_df_sorted['Index'].isin(range(0, 20))]
-    #source = ColumnDataSource(selection)
+    print(strftime("%Y-%m-%d %H:%M:%S ", localtime()), "Setting up data source for plotting.")
 
     source = ColumnDataSource(cluster_df_sorted)
+
     #
     # Plotting
     #
-
-    #output_file('test.html')
-    p = figure(#y_range=sorted(list(set(source.data['Cluster'])), key=lambda x: int(x), reverse=True),
-               plot_width=1500,
+    p = figure(plot_width=1500,
                plot_height=800,
                toolbar_location='right')
-
-    # cr = p.hbar(y='Cluster',
-    #             left='xmin',
-    #             right='xmax',
-    #             height=0.3,
-    #             line_width=4,
-    #             source=source,
-    #             fill_alpha=0.5,
-    #             line_alpha=0.5,
-    #             hover_line_color='red',
-    #             hover_fill_color='red',
-    #             hover_line_alpha=0.9,
-    #             hover_fill_alpha=0.9)
 
     cr = p.square(y='Index',
                     x='xmin',
@@ -136,11 +199,9 @@ def main(bed_file, genome_file):
     p.xaxis.axis_label = None
     p.yaxis.axis_label = 'Cluster id'
 
-    current_pos = 0
-    global current_pos
-    p.y_range = Range1d(current_pos - 1, current_pos + 21)
-    p.yaxis.ticker = list(range(current_pos, current_pos + 21))
-    p.yaxis.major_label_overrides = {str(i): '#' + str(i) + ' = ' + str(key) for key, i in cluster_index.items() if i in range(current_pos, current_pos + 20)}
+    p.y_range = Range1d(0, 20)
+    p.yaxis.ticker = list(range(0, len(cluster_index)))
+    p.yaxis.major_label_overrides = {str(i): '#' + str(i) + ' = ' + str(key).rjust(8) for key, i in cluster_index.items() if i in range(0, len(cluster_index))}
 
     p.outline_line_color = None
 
@@ -150,44 +211,86 @@ def main(bed_file, genome_file):
 
     p.xgrid.grid_line_color = None
 
-    p.title.text = "Cluster overview " + str(current_pos) + ' - ' + str(current_pos + 20) + ' of total (' + str(len(cluster_index)) + ')'
+    p.title.text = "Cluster overview " #+ str(current_pos) + ' - ' + str(current_pos + 20) + ' of total (' + str(len(cluster_index)) + ')'
 
-    show(p)
+    #show(p)
 
-    button_fwd = Button(label='Fwd', button_type='success')
-    button_rev = Button(label='Rev', button_type='success')
+    # button_fwd = Button(label='Forward 20', button_type='success')
+    # button_rev = Button(label='Reverse 20', button_type='success')
+    #
+    # def update_y_axis(change, cluster_index):
+    #     global current_pos
+    #     position = current_pos + change
+    #     if 0 <= position:
+    #         p.y_range.start += change
+    #         p.y_range.end += change
+    #         p.yaxis.ticker = list(range(position-1, position + 20))
+    #         p.yaxis.major_label_overrides = {str(i): '#' + str(i) + ' = ' + str(key).rjust(8) for key, i in cluster_index.items() if
+    #                                          i in range(position-1, position + 20)}
+    #         current_pos = position
+    #         p.title.text = "Cluster overview " + str(current_pos) + ' - ' + str(current_pos + 20) + ' of total (' + str(
+    #             len(cluster_index)) + ')'
+    #
+    #     print(current_pos)
+    #
+    # def cb_fwd():
+    #     print("FORWARD")
+    #     update_y_axis(20, cluster_index)
+    #
+    # def cb_rev():
+    #     print("REVERSE")
+    #     update_y_axis(-20, cluster_index)
+    #
+    # button_fwd.on_click(cb_fwd)
+    # button_rev.on_click(cb_rev)
+    #
+    # curdoc().add_root(layout([[p], [button_fwd, button_rev]]))
+    index = TextInput(value="O", title="Range index:")
 
-    def update_axis(change, cluster_index):
-        global current_pos
-        position = current_pos + change
-        if 0 <= position:
-            p.y_range.start += change
-            p.y_range.end += change
-            p.yaxis.ticker = list(range(position-1, position + 20))
-            p.yaxis.major_label_overrides = {str(i): '#' + str(i) + ' = ' + str(key) for key, i in cluster_index.items() if
-                                             i in range(position-1, position + 21)}
-            current_pos = position
-            p.title.text = "Cluster overview " + str(current_pos) + ' - ' + str(current_pos + 20) + ' of total (' + str(
-                len(cluster_index)) + ')'
+    def update_range(attr, old, new):
+        p.y_range.start = int(index.value)
+        p.y_range.end = int(index.value) + 20
 
-        print(current_pos)
+    index.on_change('value', update_range)
 
-    def cb_fwd():
-        print("FORWARD")
-        update_axis(20, cluster_index)
-
-    def cb_rev():
-        print("REVERSE")
-        update_axis(-20, cluster_index)
-
-    button_fwd.on_click(cb_fwd)
-    button_rev.on_click(cb_rev)
-
-    curdoc().add_root(layout([[p], [button_fwd, button_rev]]))
+    curdoc().add_root(layout([[p], [index]]))
     curdoc().title = "Test"
-#load_data(join(dirname(__file__), 'test.bed'))
-#load_data(join(dirname(__file__), 'hg38.genome'))
 
-bed_file = 'data/test.bed'
+#
+# Startup options
+#
+
+# Genome file
 genome_file = 'data/hg38.genome'
-main(bed_file=bed_file, genome_file=genome_file)
+
+if not sys.argv[1]:
+    # List available data files
+    data_files = os.listdir('data')
+    bed_files = [file_name for file_name in data_files if '.bed' in file_name]
+
+    print('AVAILABLE DATA FILES:')
+    print('-'*30)
+    for nr, file in enumerate(bed_files):
+        print(nr, ':', file)
+    print('-'*30)
+
+    file_index = int(input('Give selected file index:'))
+    bed_file = 'data/' + bed_files[file_index]
+
+else:
+    bed_file = sys.argv[1]
+
+print('CURRENT FILE = ', bed_file)
+
+print('AVAILABLE SORT OPTIONS:')
+print('-'*30)
+options = ['Cluster', 'Fragments', 'Chromosomes', 'Phased_regions']
+for nr, option in enumerate(options):
+    print(nr, ':', option)
+print('-'*30)
+
+option_index = int(input('Give selected sort option index:'))
+sorting_type = options[option_index]
+
+# Start main program
+main(bed_file_name=bed_file, genome_file_name=genome_file, sort_cluster_type=sorting_type)
